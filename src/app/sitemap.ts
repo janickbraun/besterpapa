189b955e55a3d0e169513d3715549c3973ae4475
Janick Braun <nasebuch@gmail.com>
2026-05-14 17:40:07 +0200

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.besterpapa.de'
+  const baseUrl = 'https://besterpapaderwelt.de'
  
   return [
     {
